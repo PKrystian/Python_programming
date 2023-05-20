e175123b18916ca9fdee6030d9b533f26295e60a
PKrystian <krystian.pinczak2001@gmail.com>
2023-05-20 22:59:38 +0200

    Example 8 done

--- oop/oop8.py
+++ oop/oop8.py
@@ -15,7 +15,13 @@ class Student:
         else:
             self.is_student = True
             self.school = school_name
-            self.school.add_student(self)
+            school_name.add_student(self)
+    
+    def rem_school(self):
+        if self.is_student != None:
+            self.is_student = None
+            self.school.rem_student(self)
+            self.school = None
 
 class School:
     def __init__(self, name) -> None:
@@ -26,13 +32,16 @@ class School:
         return f"{self.name}"
 
     def add_student(self, student):
-        if (student.school == self.name):
-            print(f"Student {student.first_name} {student.last_name} already attends to this school x")
-        else:
-            if student not in self.students_list:
+        if ((student not in self.students_list)):
+            if((student.is_student == False) | (str(student.school) == str(self.name))):
                 self.students_list.append(student)
-            else:
-                print(f"Student {student.first_name} {student.last_name} already attends to this school xx")
+                student.add_school(self)
+            else: print(f"Student {student.first_name} {student.last_name} already attends to {student.school} you can't add him to {self.name}")
+
+    def rem_student(self, student):
+        if student in self.students_list:
+            self.students_list.remove(student)
+            student.rem_school()
 
     def get_student_list(self):
         return [str(student) for student in self.students_list]
@@ -44,6 +53,15 @@ stud1 = Student("Jakub", "Nowak")
 stud2 = Student("Maciej", "Keqwa")
 
 sch1.add_student(stud1)
-sch1.add_student(stud1)
+sch1.add_student(stud2)
 
-stud1.add_school(sch1)
+# sch2.add_student(stud1)
+# stud1.add_school(sch1)
+# stud1.add_school(sch2)
+
+print(f"1: {sch1.get_student_list()}")
+
+sch1.rem_student(stud1)
+# stud1.rem_school()
+
+print(f"2: {sch1.get_student_list()}")
